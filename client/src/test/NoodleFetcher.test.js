013044cdb721a79ca1f1ad7e871f7b65c2a10083
Walter Scarborough <walter.scarborough@gmail.com>
2018-07-20 07:54:03 -0400

    Use spring-cloud-contract-json-reader library in client test

--- client/src/test/NoodleFetcher.test.js
+++ client/src/test/NoodleFetcher.test.js
@@ -1,3 +1,4 @@
+const readJsonContractFile = require('spring-cloud-contract-json-reader')
 const { NoodleFetcher } = require('../NoodleFetcher')
 
 describe('fetching noodles', () => {
@@ -23,27 +24,9 @@ describe('fetching noodles', () => {
 
     it('should fetch the latest noodle ratings with contracts',  async () => {
         const noodleFetcher = new NoodleFetcher()
-        const contractData = await readContractJson('shouldReturnAvailableNoodles.json')
+        const contractData = await readJsonContractFile('shouldReturnAvailableNoodles.json')
 
         const noodleResponse = await noodleFetcher.fetchNoodles()
         expect(noodleResponse).toEqual(JSON.parse(contractData))
     })
 });
-
-function readContractJson(filename) {
-    const { exec } = require('child_process');
-    const util = require('util')
-    const fs = require('fs')
-
-    const execAsPromise = util.promisify(exec)
-    const fsReadFileAsPromise = util.promisify(fs.readFile)
-
-    return execAsPromise('git rev-parse --show-toplevel')
-        .then((output) => {
-            const topLevelPath = output.stdout.replace('\n','')
-            return fsReadFileAsPromise(`${topLevelPath}/build/stubs/META-INF/com.contractdemo/contractdemo/0.0.1-SNAPSHOT/mappings/${filename}`)
-        })
-        .then((contractData) => {
-            return JSON.parse(contractData).response.body
-        })
-}
